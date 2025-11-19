3a748b0aa4c656379f51a04d77b7bfa009c790fa
saanvi548 <palvaisaanvi@gmail.com>
2025-11-19 12:04:41 +0530

    databse is not beign created

--- app/api/store/create/route.js
+++ app/api/store/create/route.js
@@ -1,7 +1,7 @@
 import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 import  prisma  from "@/lib/prisma"
-import { imagekit } from "@/configs/imageKit"
+import  imagekit  from "@/configs/imageKit"
 export async function POST(request) {
     
     try {
